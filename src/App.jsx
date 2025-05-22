66ed44b0e799001bab8f453eeebf42262a60365f
Luthfil4K <akaishuichi141516@gmail.com>
2025-05-22 15:12:48 +0800

    app jsx fix rekap individu

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
                     }
                   />
                   <Route
-                    path="/rekapIndividu"
+                    path="/rekapIndividu/:id"
                     element={
                       <PrivateRoute>
                         <RekapIndividu />
